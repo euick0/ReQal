9a9a1ec2afd7022b22fa39a32e8ec824c8db4c80
Eric Golovatenko Simões <eric2620371@gmail.com>
2026-02-09 20:58:11 +0000

    idk

---
+++ frontend/app/main/flashcards/625-words/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import FlashcardParameters from "@/app/main/flashcards/flashcardParameters";
+
+const Words = () => {
+    
+    return (
+        <div className="flex flex-col w-dvw h-dvh justify-between items-center">
+            <FlashcardParameters />
+            <div className="w-0.5 h-4/5 bg-rose-300 rounded-lg"></div>
+            <div className="">
+
+            </div>
+        </div>
+    );
+};
+
+export default Words;
---
+++ frontend/app/main/flashcards/flashcardParameters.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const FlashcardParameters = () => {
+    const languages = [
+        "English",
+        "Spanish",
+        "French",
+        "German",
+        "Chinese",
+        "Japanese",
+        "Portuguese",
+        "Russian",
+        "Italian",
+        "Korean"
+    ] as const
+    return (
+        <div className="">
+
+        </div>
+    );
+};
+
+export default FlashcardParameters;
