7d6d1f85dd0694576f06403160ae7954f7ca29e6
Eric Golovatenko Simões <eric2620371@gmail.com>
2026-03-23 16:34:09 +0000

    small fix

--- frontend/app/auth/callback/route.ts
+++ frontend/app/auth/callback/route.ts
@@ -16,5 +16,5 @@ export async function GET(request: Request) {
         return NextResponse.redirect(`${siteUrl}/auth/reset-password`);
     }
 
-    return NextResponse.redirect(`${siteUrl}/main`);
+    return NextResponse.redirect(siteUrl!);
 }
